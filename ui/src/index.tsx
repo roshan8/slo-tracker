5dd6bc32d5b1b135b63829dfce19b2ff6d5739c3
roshan.r <roshan.aloor@gmail.com>
2021-04-05 13:01:24 +0530

    added login page at the fe

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -2,15 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import LoginApp from './Login';
 import reportWebVitals from './reportWebVitals';
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+if (localStorage.getItem('IsAuthenticated') == "yes") {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+} 
+else {
+  ReactDOM.render(
+    <React.StrictMode>
+      <LoginApp />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
