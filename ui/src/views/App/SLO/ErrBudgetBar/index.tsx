cd1506dad75d826bfcbcc902ede6f336f8d7b04d
Pranav Doshetty <pranav@squadcast.com>
2021-06-09 09:15:27 +0530

    fix: length of progress bar

--- ui/src/views/App/SLO/ErrBudgetBar/index.tsx
+++ ui/src/views/App/SLO/ErrBudgetBar/index.tsx
@@ -1,40 +1,67 @@
-import React from 'react';
-import { IIncidentSummary } from '../../../../core/interfaces/IIncident';
+import React from "react";
+import { Popover } from "antd";
+import { IIncidentSummary } from "../../../../core/interfaces/IIncident";
 
-import './errBudgetBar.css'
+import "./errBudgetBar.css";
 
 interface IProps {
 	data: IIncidentSummary[];
 }
 
-const ErrBudgetBar: React.FC<IProps> = ({ data }) => {
 
+
+const ErrBudgetBar: React.FC<IProps> = ({ data }) => {
 	const progBar = (() => {
-		let prog = data.map(function (val, i) {
-			const randColor = Math.floor(Math.random()*16777215).toString(16)
-			return (
-				<div className="bar" style={{ 'width': val.value + '%', 'backgroundColor': '#' + randColor}} key={i}>
-					
+		let sum = 0;
+		data.forEach((x) => {
+			sum += x.value;
+		});
+		let prog = data.map((val, i) => {
+			const w = (val.value / sum) * 100;
+			const randColor = Math.floor(Math.random() * 16777215).toString(16);
+			const legend = (
+				<div>
+					<span
+						className="dot"
+						style={{ color: "#" + randColor, marginRight: "10px" }}
+					>
+						●
+					</span>
+					<span
+						className="label"
+						style={{ marginRight: "10px", marginBottom: "10px" }}
+					>
+						{val.label}
+					</span>
+					<span className="perc">{w.toFixed(2)}%</span>
 				</div>
-			)
+			);
+			return (
+				<Popover content={legend}>
+					<div
+						className="bar"
+						style={{ width: w + "%", backgroundColor: "#" + randColor }}
+						key={i}
+					></div>
+				</Popover>
+			);
 		});
 
 		return (
 			<div className="multi-progress-bar">
-				<div className="bars">
-					{prog}
-				</div>
+				<div className="bars">{prog}</div>
 			</div>
 		);
 	})();
 
 	return (
 		<div>
-			{data.length ? progBar: (
+			{data.length ? (
+				progBar
+			) : (
 				<div
 					style={{
 						textAlign: "center",
-						paddingTop: "9rem",
 						fontWeight: 500,
 						fontSize: "24px",
 					}}
